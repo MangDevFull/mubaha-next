dd198780e9e83c6a741fda82c1ef9230e8d67b2b
mangnb <92033339+mangnb@users.noreply.github.com>
2022-02-19 17:16:04 +0700

    new layout login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -20,7 +20,7 @@ export default function LoginPage() {
 
   const [isVerifyPhone, setIsVerifyPhone] = useState(false);
   const [isNotValidPhone, setIsNotValidPhone] = useState(true);
-  const [isCreatePassword,setIsCreatePassword] = useState(false);
+  const [isCreatePassword, setIsCreatePassword] = useState(false);
   const [isInvalid, setInvalid] = useState(false)
   const [message, setMessage] = useState('')
   const inputPhone = useRef();
@@ -28,7 +28,7 @@ export default function LoginPage() {
   const [showPass, setShowPass] = useState('block');
   const [hidePass, setHidePass] = useState('none')
   const [inputValues, setInputValues] = useState('password')
-  const [phone,setPhone]= useState("")
+  const [phone, setPhone] = useState("")
   const router = useRouter();
 
   const handleShowPassword = () => {
@@ -44,7 +44,7 @@ export default function LoginPage() {
 
   useEffect(() => {
     inputPhone.current.focus()
-  },[])
+  }, [])
 
   const checkPhone = (e) => {
     const phone = e.target.value
@@ -67,10 +67,10 @@ export default function LoginPage() {
 
   const getValueForm = async (e) => {
     e.preventDefault()
-    if(inputPassword.current.value=="") {
+    if (inputPassword.current.value == "") {
       setMessage("Mật khẩu không được bỏ trống");
-        setInvalid(true)
-    }else{
+      setInvalid(true)
+    } else {
       const res = await signIn("mubaha-login", {
         phone: inputPhone.current.value,
         password: inputPassword.current.value,
@@ -80,9 +80,9 @@ export default function LoginPage() {
         router.push('/')
       } else {
         const data = JSON.parse(res.error)
-          setMessage(data.message);
-          setInvalid(true)
-          setIsCreatePassword(false)
+        setMessage(data.message);
+        setInvalid(true)
+        setIsCreatePassword(false)
       }
     }
   }
@@ -115,7 +115,7 @@ export default function LoginPage() {
           const response = await API.instance.post("/auth/login-otp", params);
           const data = response.data;
           if (data.status == 200) {
-            console.log("data",data);
+            console.log("data", data);
             setIsVerifyPhone(true);
             setMessage("");
           } else {
@@ -132,94 +132,97 @@ export default function LoginPage() {
       <Head>
         <title>Đăng nhập với mật khẩu</title>
       </Head>
-      { !isVerifyPhone 
-      && 
-      <div className="login-page container-fluit">
+      {!isVerifyPhone
+        &&
+        <div className="login-page container-fluit">
+          <Row className="background_login d-flex justify-content-center">
+            <div className="right-login margin-form d-flex">
+              <div className="" style={{width:"50%"}}>
+                <ImageAuthen />
+              </div>
+              <div className="theme-card login_form-right " style={{width:"50%"}}>
+                <div className="justify-content-center mt-4 mb-5 ml-3 mr-3">
+                  <h3 className="text-center">Đăng Nhập</h3>
+                </div>
+                <Form className="theme-form ml-3 mr-3" onSubmit={getValueForm}>
+                  {isInvalid &&
+                    <Alert style={{ textAlign: 'center', height: '40px' }} variant={'danger'}>
+                      {message}
+                    </Alert>
+                  }
+                  {isCreatePassword &&
+                    <Alert style={{ textAlign: 'center', height: '70px' }} variant={'danger'}>
+                      <span>{message} </span>
+                      <br></br>
+                      <br></br>
+                      <a href="#" onClick={createPassword}>Đặt mật khẩu</a>
+                    </Alert>
+                  }
+                  <div className="form-group mb-1">
+                    <input ref={inputPhone}
+                      onChange={checkPhone}
+                      type="text" className="form-control"
+                      placeholder="Nhập số điện thoại của bạn" required="" />
+                  </div>
+                  <div className="form-group mb-1">
+                    <div className="d-flex">
+                      <input type={inputValues} className="form-control"
+                        ref={inputPassword}
+                        placeholder="Nhập mật khẩu của bạn" required="" />
+                      <div onClick={handleShowPassword} style={{ display: showPass }} className="hide-show-password">
+                        <AiFillEye className="icon-password" />
+                      </div>
+                      <div onClick={handlHidePassword} style={{ display: hidePass }} className="hide-show-password">
+                        <AiFillEyeInvisible className="icon-password" />
+                      </div>
+                    </div>
+                    <div className="d-flex justify-content-between mb-5">
+                      <div >
+                        <Link href="#">
+                          <a className="text-link text-secondary">Quên mật khẩu?</a>
+                        </Link>
+                      </div>
+                      <div>
+                        <Link href="/auth/login-otp">
+                          <a className="text-link text-primary">Đăng nhập SMS</a>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <button type='submit' disabled={isNotValidPhone} className="btn btn-solid btn-login">Đăng nhập</button>
+                  </div>
+                </Form>
+                <div className="login-social mx-auto">
+                  <h5 className="text-or">HOẶC TIẾP TỤC VỚI</h5>
+                  <LoginSocail />
+                </div>
+                <Row className='register d-flex justify-content-center  ml-3 mr-3 mb-5'>
+                  <div className="">
+                    <Link href="/auth/register">
+                      <a className="text-primary" >Tạo một tài khoản mới</a>
+                    </Link>
+                  </div>
+                </Row>
+              </div>
+            </div>
+          </Row>
+          <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}>
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+            </symbol>
+            <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+            </symbol>
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </symbol>
+          </svg>
 
-<Row className="background_login">
-  <Col lg="7">
-    <ImageAuthen />
-  </Col>
-  <Col lg="4" className="right-login mt-5 mb-5 container" >
-    <div className="theme-card login_form" >
-      <h5>Đăng Nhập</h5>
-      <Form className="theme-form" onSubmit={getValueForm}>
-        {isInvalid &&
-          <Alert style={{ textAlign: 'center', height: '40px' }} variant={'danger'}>
-            {message}
-          </Alert>
-        }
-        {isCreatePassword &&
-          <Alert style={{ textAlign: 'center', height: '70px' }} variant={'danger'}>
-            <span>{message} </span>
-            <br></br>
-            <br></br>
-            <a href="#" onClick={createPassword}>Đặt mật khẩu</a>
-          </Alert>
-        }
-        <div className="form-group mb-1">
-          <input ref={inputPhone}
-            onChange={checkPhone}
-            type="text" className="form-control" placeholder="Nhập số điện thoại của bạn" required="" />
         </div>
-        <div className="form-group d-flex mb-1">
-          <input type={inputValues} className="form-control"
-            ref={inputPassword}
-            placeholder="Nhập mật khẩu của bạn" required="" />
-          <div onClick={handleShowPassword} style={{ display: showPass }} className="hide-show-password">
-            <AiFillEye className="icon-password" />
-          </div>
-          <div onClick={handlHidePassword} style={{ display: hidePass }} className="hide-show-password">
-            <AiFillEyeInvisible className="icon-password" />
-          </div>
-        </div>
-        <button type='submit' disabled={isNotValidPhone} style={{ width: '100%', backgroundColor: '#f89922' }} className="btn btn-solid">Đăng nhập</button>
-        <div className="d-flex justify-content-between" style={{ paddingTop: '10px' }}>
-          <div >
-            <Link href="#">
-              <a className="text-link">Quên mật khẩu</a>
-            </Link>
-          </div>
-          <div>
-            <Link href="/auth/login-otp">
-              <a className="text-link">Đăng nhập SMS</a>
-            </Link>
-          </div>
-        </div>
-      </Form>
-      <div className="login-social mx-auto">
-        <h5 className="text-or">HOẶC TIẾP TỤC VỚI</h5>
-        <LoginSocail />
-        <Row className='register'>
-          <div className="mx-auto"> 
-            <p className=''><span>Bạn chưa có tài khoản? </span>
-              <Link href="/auth/register">
-                <a >Đăng ký</a>
-              </Link>
-            </p>
-          </div>
-        </Row>
-      </div>
-    </div>
-  </Col>
-  <Col lg="1"></Col>
-</Row>
-<svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}>
-  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-  </symbol>
-  <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-  </symbol>
-  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-  </symbol>
-</svg>
-
-</div>
       }
-    
-      { isVerifyPhone && <Otp  phone={phone} type={otpEnums.CREATE_PASSWORD} />}
+
+      {isVerifyPhone && <Otp phone={phone} type={otpEnums.CREATE_PASSWORD} />}
       {/*Section ends*/}
     </>
   )
