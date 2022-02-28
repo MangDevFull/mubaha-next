5d9fc1be4184005f83eb131afc7d6e840a7934d4
mangnb <92033339+mangnb@users.noreply.github.com>
2022-02-28 09:31:32 +0700

    update img authen

--- components/authen/ImgaeAuthen.js
+++ components/authen/ImgaeAuthen.js
@@ -3,7 +3,7 @@ import styles from '@/styles/authen.module.css'
 export default function ImageAuthen(){
   return(
     <>
-<Image className={styles.imgAuthen} src="/assets/icon/452x582-01.png" layout="fill" />
+<Image className={styles.imgAuthen} src="/assets/icon/login.jpeg" layout="fill" />
     </>
   )
 }
