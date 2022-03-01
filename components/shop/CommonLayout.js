c87e0dd79de050c4c76cbfc3ebad17f361e84c9c
NMQuang <minquangfpt@gmail.com>
2022-03-01 17:28:07 +0700

    update filter

--- components/shop/CommonLayout.js
+++ components/shop/CommonLayout.js
@@ -6,7 +6,7 @@ const CommonLayout = ({ children, title, parent, subTitle }) => {
     <>
       
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Vendor Profile</title>
+        <title>{title}</title>
       {/* breadcrumb start */}
       <Breadcrumbs title={title} parent={parent} subTitle={subTitle}/>
       <>{children}</>
