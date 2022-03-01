6d7548ba4f6f237c7f84a6c2b1d549f41fa95cc2
NMQuang <minquangfpt@gmail.com>
2022-03-01 17:30:34 +0700

    update CommonLayout

--- components/shop/CommonLayout.js
+++ components/shop/CommonLayout.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Breadcrumbs from "../common/widgets/Breadcrubs.js"
+import Breadcrumbs from "../common/widgets/Breadcrubs.js";
 
 const CommonLayout = ({ children, title, parent, subTitle }) => {
   return (
     <>
-      
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{title}</title>
+      </Head>
+
       {/* breadcrumb start */}
-      <Breadcrumbs title={title} parent={parent} subTitle={subTitle}/>
+      <Breadcrumbs title={title} parent={parent} subTitle={subTitle} />
       <>{children}</>
     </>
   );
