e030c671078c63a8511bd9bdc5ef95c29b5a12a6
Phước Nguyễn <mangnb30062001@gmail.com>
2022-03-01 17:12:19 +0700

    update backend

---
+++ pages/vendor/products/add-product.js
@@ -0,0 +1,10 @@
+import AddProduct from '@/components/backend/products/physical/add-product.js'
+export default function AddProductPage(){
+    return (
+        <>
+        <AddProduct />
+        </>
+    )
+}
+
+
