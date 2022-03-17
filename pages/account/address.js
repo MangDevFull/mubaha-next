c53118053ffb4ee30a442849c956dc54329704bf
mangnb <92033339+mangnb@users.noreply.github.com>
2022-03-17 09:43:43 +0700

    update

--- pages/account/address.js
+++ pages/account/address.js
@@ -11,17 +11,17 @@ import {
   Alert,
 } from "reactstrap";
 import Layout from "@/components/LayoutCart";
-import CommonLayout from "../components/shop/CommonLayout";
-import styles from "../styles/account.module.css";
+import CommonLayout from "../../components/shop/CommonLayout";
+import styles from "@/styles/account.module.css";
 
 const Account = () => {
   const [accountInfo, setAccountInfo] = useState(false);
   const [show, setShow] = useState(false);
   const [message, setMessage] = useState("");
   const [showMessage, setShowMessage] = useState(false);
+  const [address,setAddress] = useState()
   const handleClose = () => setShow(false);
   const handleShow = () => {
-    console.log("check");
     setShow(true);
     setMessage("");
     setShowMessage(false);
@@ -47,12 +47,6 @@ const Account = () => {
                       <li className="active">
                         <a href="#">Địa chỉ</a>
                       </li>
-                      <li>
-                        <a href="#">Address Book</a>
-                      </li>
-                      <li>
-                        <a href="#">My Orders</a>
-                      </li>
                     </ul>
                   </div>
                 </div>
@@ -65,7 +59,7 @@ const Account = () => {
                         <h2>Địa chỉ của tôi</h2>
                       </div>
                       <div className={`${styles.add_address}`}>
-                        <button onClick={handleShow}>Thêm địa chỉ</button>
+                        <button onClick={handleShow} style={{width:"auto"}}><i className="fa fa-solid fa-plus mr-1"></i>Thêm địa chỉ</button>
                       </div>
                     </div>
 
