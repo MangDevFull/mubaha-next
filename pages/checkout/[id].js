f400d6b83303fad21df108e4101fabef6c0cb283
NMQuang <minquangfpt@gmail.com>
2022-03-22 09:35:27 +0700

    update checout

--- pages/checkout/[id].js
+++ pages/checkout/[id].js
@@ -3,6 +3,7 @@ import CommonLayout from "../../components/shop/CommonLayout";
 import CheckoutPage from "../../components/common/CheckoutPage";
 import HeaderAuthen from "@/components/authen/HeaderAuthen.js";
 import Footer from "@/components/Footer.js";
+import { getSession } from "next-auth/react";
 const Checkout = () => {
   return (
     <>
@@ -16,3 +17,18 @@ const Checkout = () => {
 };
 
 export default Checkout;
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  const session = await getSession(context);
+  const response = await fetch(`${process.env.API_ORDER_URL}/checkout?carts=${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer" + session.accessToken,
+    },
+  });
+  return {
+    props: {},
+  };
+}
--- pages/checkout/_middleware.js
+++ pages/checkout/_middleware.js
@@ -1,12 +1,10 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 export async function middleware(req) {
-  if (req.page.name !== "/checkout") {
-    const token = await getToken({
-      req,
-      secret: process.env.JWT_SECRET,
-    });
-    if (token) return NextResponse.redirect("/");
-    return NextResponse.next();
-  }
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+  });
+  if (!token) return NextResponse.redirect("/auth/login");
+  return NextResponse.next();
 }
