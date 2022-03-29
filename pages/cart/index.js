79c34bf29e8013efa50c7b0b3cc1fa0ff9474000
Phước Nguyễn <mangnb30062001@gmail.com>
2022-03-29 11:07:27 +0700

    up

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -30,70 +30,50 @@ export async function getServerSideProps(ctx) {
       'Authorization': 'Bearer ' + session.accessToken
     },
   })
-
-  const data = await res.json()
-  const results = data.data.cartItems.grouped
   let unActive = 0
-  const fullP = results.map(product => {
-    let countActive = 0
-    let countOutOfStocks = 0
-    let countChange = 0
-    let countSelection = 0
-    const d = product.products.map((p, index) => {
-      if(p.product.status === productStatusEnum.DISABLE ){
-        countActive += 1
-        unActive+=1
-      }
-      if(query===p._id){
-        countSelection+=1
-      }
-      if(p.isChanged){
-        countChange += 1
-      }
-      let value = {
-        quantity: p.amount,
-        name: p.product.name,
-        currencySymbol: p.product.currencySymbol,
-        slug: p.product.slug,
-        cartID: p._id,
-        selected: query === p._id,
-        productID: p.product._id,
-        discount: p.product.discount,
-        status: p.product.status,
-        price: p.price,
-        discount: p.discount,
-        isChanged: p.isChanged
-      }
-      if (p.selectedVariant != null && p.selectedAttribute == null) {
-        value = {
-          ...value,
-          variantLable: p.product.variantLabel,
-          variant: p.selectedVariant,
-          variants: p.product.variants,
+  const data = await res.json()
+  let send = { relatedProducts: data.data.relatedProducts }
+  if (data.data.cartItems !== null) {
+    const results = data.data.cartItems.grouped
+    const final = results.map(product => {
+      let countActive = 0
+      let countOutOfStocks = 0
+      let countChange = 0
+      let countSelection = 0
+      const d = product.products.map((p, index) => {
+        if (p.product.status === productStatusEnum.DISABLE) {
+          countActive += 1
+          unActive += 1
         }
-        if (p.selectedVariant.stock.quantity == 0 && p.product.status !== productStatusEnum.DISABLE) {
-          countOutOfStocks+=1
-          value = {
-            ...value,
-            isOutOfStock: true,
-          }
-        } else {
-          value = {
-            ...value,
-            isOutOfStock: false,
-          }
+        if (query === p._id) {
+          countSelection += 1
         }
-      } else if (p.selectedVariant != null && p.selectedAttribute != null) {
+        if (p.isChanged) {
+          countChange += 1
+        }
+        let value = {
+          quantity: p.amount,
+          name: p.product.name,
+          currencySymbol: p.product.currencySymbol,
+          slug: p.product.slug,
+          cartID: p._id,
+          selected: query === p._id,
+          productID: p.product._id,
+          discount: p.product.discount,
+          status: p.product.status,
+          price: p.price,
+          discount: p.discount,
+          isChanged: p.isChanged
+        }
+        if (p.selectedVariant != null && p.selectedAttribute == null) {
           value = {
             ...value,
-            variant: p.selectedVariant,
-            attr: p.selectedAttribute,
-            variants: p.product.variants,
             variantLable: p.product.variantLabel,
-            attributeLabel: p.product.attributeLabel,
+            variant: p.selectedVariant,
+            variants: p.product.variants,
           }
-          if (p.selectedAttribute.stock.quantity == 0 && p.product.status !== productStatusEnum.DISABLE) {
-            countOutOfStocks+=1
+          if (p.selectedVariant.stock.quantity == 0 && p.product.status !== productStatusEnum.DISABLE) {
+            countOutOfStocks += 1
             value = {
               ...value,
               isOutOfStock: true,
@@ -104,40 +84,72 @@ export async function getServerSideProps(ctx) {
               isOutOfStock: false,
             }
           }
-      } else {
-        value = {
-          ...value,
-          price: p.product.price,
-          image: p.product.media.featuredImage
-        }
-        if (p.product.stock.quantity == 0) {
-          count+=1
+        } else if (p.selectedVariant != null && p.selectedAttribute != null) {
           value = {
             ...value,
-            isOutOfStock: true,
+            variant: p.selectedVariant,
+            attr: p.selectedAttribute,
+            variants: p.product.variants,
+            variantLable: p.product.variantLabel,
+            attributeLabel: p.product.attributeLabel,
+          }
+          if (p.selectedAttribute.stock.quantity == 0 && p.product.status !== productStatusEnum.DISABLE) {
+            countOutOfStocks += 1
+            value = {
+              ...value,
+              isOutOfStock: true,
+            }
+          } else {
+            value = {
+              ...value,
+              isOutOfStock: false,
+            }
           }
         } else {
           value = {
             ...value,
-            isOutOfStock: false,
+            price: p.product.price,
+            image: p.product.media.featuredImage
+          }
+          if (p.product.stock.quantity == 0) {
+            count += 1
+            value = {
+              ...value,
+              isOutOfStock: true,
+            }
+          } else {
+            value = {
+              ...value,
+              isOutOfStock: false,
+            }
           }
         }
+        return value
+      })
+      return {
+        vendor: product.vendor,
+        selected: product.products.length - countChange - countOutOfStocks - countOutOfStocks === countSelection,
+        totalDocs: product.products.length,
+        products: d,
+        count: countActive + countChange + countOutOfStocks,
       }
-      return value
     })
-    return {
-      vendor: product.vendor,
-      selected:product.products.length - countChange -countOutOfStocks -countOutOfStocks === countSelection ,
-      totalDocs: product.products.length,
-      products: d,
-      count: countActive + countChange + countOutOfStocks,
+    send = {
+      ...send, fullP: final,
+      page: data?.data?.cartItems.page,
+      totalPage: data?.data?.cartItems.totalPages, totalDocs: data?.data?.cartItems.totalDocs,
+      unActive: unActive
     }
-  })
+  } else {
+    send = {
+      ...send,
+      fullP: []
+    }
+  }
+
   return {
     props: {
-      data:{fullP:fullP, page: data.data.cartItems.page,
-        totalPage: data.data.cartItems.totalPages, totalDocs: data.data.cartItems.totalDocs,
-        relatedProducts: data.data.relatedProducts,unActive:unActive}
+      data: send
     }
   };
 }
